c5d2e6ccb44e461705e3abd1607c390982f14b7d
Thibaud-Guadagna <thibaud.guadagna@gmail.com>
2025-05-22 09:17:46 +0200

    Ajout du router et modifs css

--- Junior-Hackers/src/main.tsx
+++ Junior-Hackers/src/main.tsx
@@ -2,15 +2,35 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { RouterProvider, createBrowserRouter } from "react-router";
+import Home from "./pages/home.tsx";
+import Game from "./pages/game.tsx";
+import BoardGame from "./components/boardGame.tsx";
 
-const rootEl = document.getElementById("root");
+const router = createBrowserRouter([
+	{
+		element: <App />,
 
-if (rootEl) {
-	createRoot(rootEl).render(
-		<StrictMode>
-			<App />
-		</StrictMode>,
-	);
-} else {
-	console.error("Impossible de trouver l'élément #root");
+		children: [
+			{
+				path: "/",
+				element: <Home />,
+			},
+			{
+				path: "/Game",
+				element: <Game />,
+			},
+		],
+	},
+]);
+
+const rootElement = document.getElementById("root");
+if (rootElement == null) {
+	throw new Error(`Your HTML Document should contain a <div id="root"></div>`);
 }
+
+createRoot(rootElement).render(
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>,
+);
--- Junior-Hackers/src/pages/home.tsx
+++ Junior-Hackers/src/pages/home.tsx
@@ -2,8 +2,10 @@ import ModalExample from "../components/modal";
 import AnimatedTitle from "../components/animatedTitle";
 import AlgobotWithSpeech from "../components/AlgobotWhiteSpeech";
 import FloatingElements from "../components/floatingElements";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+	const navigate = useNavigate();
 	return (
 		<main className="min-h-screen flex flex-col  justify-center text-center px-4">
 			<FloatingElements />
@@ -19,6 +21,7 @@ function Home() {
 			<section className="flex justify-around gap-4 mt-10 md:mt-20">
 				<button
 					type="button"
+					onClick={() => navigate("/Game")}
 					className="border-2 border-gray-800 text-gray-800 font-semibold rounded-sm px-4 py-2
              transition-transform duration-150 ease-in-out
              hover:-translate-y-1 hover:shadow-md hover:border-gray-900 hover:text-gray-900
